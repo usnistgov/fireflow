5fedb5f3cad12f1f385e2c72aae21cb2167ac0ac
ndwarshuis <ndwar@yavin4.ch>
2025-07-31 10:10:42 -0400

    REF group core tests under class

--- pyreflow/tests/test_read_write.py
+++ pyreflow/tests/test_read_write.py
@@ -200,140 +200,130 @@ all_core = pytest.mark.parametrize(
 )
 
 
-# all of these attributes should be either None or a positive integer
-@all_blank_core
-@pytest.mark.parametrize("attr", ["abrt", "lost"])
-def test_core_metaroot_opt_int(attr: str, core: AnyCore) -> None:
-    good = 420
-    assert getattr(core, attr) is None
-    setattr(core, attr, good)
-    assert getattr(core, attr) == good
-    with pytest.raises(TypeError):
-        setattr(core, attr, "420")
-    with pytest.raises(TypeError):
-        setattr(core, attr, -420)
+class TestCore:
+    # all of these attributes should be either None or a positive integer
+    @all_blank_core
+    @pytest.mark.parametrize("attr", ["abrt", "lost"])
+    def test_metaroot_opt_int(self, attr: str, core: AnyCore) -> None:
+        good = 420
+        assert getattr(core, attr) is None
+        setattr(core, attr, good)
+        assert getattr(core, attr) == good
+        with pytest.raises(TypeError):
+            setattr(core, attr, "420")
+        with pytest.raises(TypeError):
+            setattr(core, attr, -420)
 
+    # all of these attributes should be either None or a string
+    @all_blank_core
+    @pytest.mark.parametrize(
+        "attr",
+        ["cells", "com", "exp", "fil", "inst", "op", "proj", "smno", "src", "sys"],
+    )
+    def test_metaroot_opt_str(self, attr: str, core: AnyCore) -> None:
+        good = "spongebob"
+        assert getattr(core, attr) is None
+        setattr(core, attr, good)
+        assert getattr(core, attr) == good
+        with pytest.raises(TypeError):
+            setattr(core, attr, 3.14)
 
-# all of these attributes should be either None or a string
-@all_blank_core
-@pytest.mark.parametrize(
-    "attr", ["cells", "com", "exp", "fil", "inst", "op", "proj", "smno", "src", "sys"]
-)
-def test_core_metaroot_opt_str(attr: str, core: AnyCore) -> None:
-    good = "spongebob"
-    assert getattr(core, attr) is None
-    setattr(core, attr, good)
-    assert getattr(core, attr) == good
-    with pytest.raises(TypeError):
-        setattr(core, attr, 3.14)
+    # these should be time objects
+    @all_blank_core
+    @pytest.mark.parametrize("attr", ["btim", "etim"])
+    def test_time(self, attr: str, core: AnyCore) -> None:
+        good = time(23, 58)
+        assert getattr(core, attr) is None
+        setattr(core, attr, good)
+        assert getattr(core, attr) == good
+        with pytest.raises(TypeError):
+            setattr(core, attr, "thermonuclear war")
 
+    @all_blank_core
+    def test_date(self, core: AnyCore) -> None:
+        good = date(1991, 8, 25)
+        assert core.date is None
+        core.date = good
+        assert core.date == good
+        with pytest.raises(TypeError):
+            core.date = cast(date, "Apr 1, 1976")
 
-# these should be time objects
-@all_blank_core
-@pytest.mark.parametrize("attr", ["btim", "etim"])
-def test_time(attr: str, core: AnyCore) -> None:
-    good = time(23, 58)
-    assert getattr(core, attr) is None
-    setattr(core, attr, good)
-    assert getattr(core, attr) == good
-    with pytest.raises(TypeError):
-        setattr(core, attr, "thermonuclear war")
+    @all_core
+    def test_trigger(self, core: AnyCore) -> None:
+        assert core.trigger is None
+        tr = (LINK_NAME, 0)
+        core.trigger = tr
+        assert core.trigger == tr
 
+    @all_core
+    def test_trigger_threshold(self, core: AnyCore) -> None:
+        tr = (LINK_NAME, 0)
+        core.trigger = tr
+        assert core.trigger == tr
+        core.set_trigger_threshold(1)
+        assert core.trigger == (LINK_NAME, 1)
 
-@all_blank_core
-def test_date(core: AnyCore) -> None:
-    good = date(1991, 8, 25)
-    assert core.date is None
-    core.date = good
-    assert core.date == good
-    with pytest.raises(TypeError):
-        core.date = cast(date, "Apr 1, 1976")
+    @all_blank_core
+    def test_trigger_bad(self, core: AnyCore) -> None:
+        with pytest.raises(TypeError):
+            core.trigger = cast(Trigger, "over,9000")
 
+    @all_blank_core
+    def test_trigger_nolink(self, core: AnyCore) -> None:
+        with pytest.raises(pf.PyreflowException):
+            core.trigger = (Shortname("harold"), 0)
 
-@all_core
-def test_trigger(core: AnyCore) -> None:
-    assert core.trigger is None
-    tr = (LINK_NAME, 0)
-    core.trigger = tr
-    assert core.trigger == tr
+    @all_core
+    def test_par(self, core: AnyCore) -> None:
+        assert core.par == 1
 
+    @all_core
+    def test_shortnames(self, core: AnyCore) -> None:
+        assert core.all_shortnames == [LINK_NAME]
+        new_name = Shortname("I can haz IP")
+        core.all_shortnames = [new_name]
+        assert core.all_shortnames == [new_name]
+        with pytest.raises(ValueError):
+            core.all_shortnames = [Shortname("I,can,haz,IP")]
 
-@all_core
-def test_trigger_threshold(core: AnyCore) -> None:
-    tr = (LINK_NAME, 0)
-    core.trigger = tr
-    assert core.trigger == tr
-    core.set_trigger_threshold(1)
-    assert core.trigger == (LINK_NAME, 1)
+    @all_core
+    def test_longnames(self, core: AnyCore) -> None:
+        assert core.longnames == [None]
+        new_name = "I can haz IP"
+        core.longnames = [new_name]
+        assert core.longnames == [new_name]
+        with pytest.raises(TypeError):
+            core.longnames = [cast(str, 42)]
 
+    # each of these should be strings or None
+    @all_core
+    @pytest.mark.parametrize(
+        "get, set",
+        [(x, f"set_{x}") for x in ["filters", "percents_emitted", "detector_types"]],
+    )
+    def test_meas_opt_strs(self, get: str, set: str, core: AnyCore) -> None:
+        assert getattr(core, get) == [(1, None)]
+        new = "bla"
+        getattr(core, set)([new])
+        assert getattr(core, get) == [(1, new)]
+        with pytest.raises(TypeError):
+            getattr(core, set)([42])
 
-@all_blank_core
-def test_trigger_bad(core: AnyCore) -> None:
-    with pytest.raises(TypeError):
-        core.trigger = cast(Trigger, "over,9000")
-
-
-@all_blank_core
-def test_trigger_nolink(core: AnyCore) -> None:
-    with pytest.raises(pf.PyreflowException):
-        core.trigger = (Shortname("harold"), 0)
-
-
-@all_core
-def test_par(core: AnyCore) -> None:
-    assert core.par == 1
-
-
-@all_core
-def test_shortnames(core: AnyCore) -> None:
-    assert core.all_shortnames == [LINK_NAME]
-    new_name = Shortname("I can haz IP")
-    core.all_shortnames = [new_name]
-    assert core.all_shortnames == [new_name]
-    with pytest.raises(ValueError):
-        core.all_shortnames = [Shortname("I,can,haz,IP")]
-
-
-@all_core
-def test_longnames(core: AnyCore) -> None:
-    assert core.longnames == [None]
-    new_name = "I can haz IP"
-    core.longnames = [new_name]
-    assert core.longnames == [new_name]
-    with pytest.raises(TypeError):
-        core.longnames = [cast(str, 42)]
-
-
-# each of these should be strings or None
-@all_core
-@pytest.mark.parametrize(
-    "get, set",
-    [(x, f"set_{x}") for x in ["filters", "percents_emitted", "detector_types"]],
-)
-def test_core_meas_opt_strs(get: str, set: str, core: AnyCore) -> None:
-    assert getattr(core, get) == [(1, None)]
-    new = "bla"
-    getattr(core, set)([new])
-    assert getattr(core, get) == [(1, new)]
-    with pytest.raises(TypeError):
-        getattr(core, set)([42])
-
-
-# each of these should be a non-negative float
-@all_core
-@pytest.mark.parametrize(
-    "get, set",
-    [(x, f"set_{x}") for x in ["powers", "detector_voltages"]],
-)
-def test_core_meas_opt_floats(get: str, set: str, core: AnyCore) -> None:
-    assert getattr(core, get) == [(1, None)]
-    new = 0.5
-    getattr(core, set)([new])
-    assert getattr(core, get) == [(1, new)]
-    newer = 0.0
-    getattr(core, set)([newer])
-    assert getattr(core, get) == [(1, newer)]
-    with pytest.raises(TypeError):
-        getattr(core, set)([-1.0])
-    with pytest.raises(TypeError):
-        getattr(core, set)(["pickle rick"])
+    # each of these should be a non-negative float
+    @all_core
+    @pytest.mark.parametrize(
+        "get, set",
+        [(x, f"set_{x}") for x in ["powers", "detector_voltages"]],
+    )
+    def test_meas_opt_floats(self, get: str, set: str, core: AnyCore) -> None:
+        assert getattr(core, get) == [(1, None)]
+        new = 0.5
+        getattr(core, set)([new])
+        assert getattr(core, get) == [(1, new)]
+        newer = 0.0
+        getattr(core, set)([newer])
+        assert getattr(core, get) == [(1, newer)]
+        with pytest.raises(TypeError):
+            getattr(core, set)([-1.0])
+        with pytest.raises(TypeError):
+            getattr(core, set)(["pickle rick"])

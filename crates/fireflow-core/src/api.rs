b3af40d4a70e594074b62c00ac0b188b29cd657a
ndwarshuis <ndwar@yavin4.ch>
2025-09-05 11:17:37 -0400

    FIX boolean flip

--- crates/fireflow-core/src/api.rs
+++ crates/fireflow-core/src/api.rs
@@ -1107,7 +1107,7 @@ where
     match version {
         Version::FCS2_0 => Tentative::new1(None),
         Version::FCS3_0 | Version::FCS3_1 => KeyedReqSegment::get_mult(kws, &seg_conf).map_or_else(
-            |es| Tentative::new_either(None, es.into(), conf.allow_missing_stext),
+            |es| Tentative::new_either(None, es.into(), !conf.allow_missing_stext),
             |t| Tentative::new1(Some(t)),
         ),
         Version::FCS3_2 => KeyedOptSegment::get(kws, &seg_conf).warnings_into(),

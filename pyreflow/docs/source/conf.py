c0625a40e0169d6a40ca6d514a76fba8d4fb1bd1
njd2 <njd2@nist.gov>
2025-09-12 11:32:33 -0400

    ADD nist branding

--- pyreflow/docs/source/conf.py
+++ pyreflow/docs/source/conf.py
@@ -20,16 +20,16 @@ extensions = [
     "sphinx.ext.autodoc",
     "sphinx_toolbox.more_autodoc.autonamedtuple",
     "sphinx.ext.graphviz",
+    "sphinx.ext.githubpages",
 ]
 
 templates_path = ["_templates"]
 exclude_patterns = []
 
-
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "alabaster"
+html_theme = 'nature'
 html_static_path = ["_static"]
 
 autodoc_typehints = "description"

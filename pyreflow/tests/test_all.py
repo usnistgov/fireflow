886a12e96676af433b956ad8a8186ede029cc9ca
njd2 <njd2@nist.gov>
2025-12-05 12:31:31 -0500

    FIX subpattern dict bug

--- pyreflow/tests/test_all.py
+++ pyreflow/tests/test_all.py
@@ -18,11 +18,103 @@ from pyreflow.typing import (
     AppliedGates2_0,
     AppliedGates3_0,
     AppliedGates3_2,
+    KeyPatterns,
 )
 import pyreflow as pf
 import polars as pl
 
 from .conftest import lazy_fixture
+
+
+_DEFAULT_CORRECTION = (0, 0)
+_DEFAULT_KEY_PATTERNS: KeyPatterns = ([], [])
+_DEFAULT_TIME_MEAS_PATTERN = "^(TIME|Time)$"
+
+HEADER_DEFAULT_CONFIG: dict[str, Any] = {
+    "text_correction": _DEFAULT_CORRECTION,
+    "data_correction": _DEFAULT_CORRECTION,
+    "analysis_correction": _DEFAULT_CORRECTION,
+    "other_corrections": [],
+    "max_other": None,
+    "other_width": 8,
+    "squish_offsets": False,
+    "allow_negative": False,
+    "truncate_offsets": False,
+}
+
+FLAT_DEFAULT_CONFIG: dict[str, Any] = {
+    "version_override": None,
+    "supp_text_correction": _DEFAULT_CORRECTION,
+    "allow_overlapping_supp_text": False,
+    "ignore_supp_text": False,
+    "use_literal_delims": False,
+    "allow_non_ascii_delim": False,
+    "allow_missing_final_delim": False,
+    "allow_nonunique": False,
+    "allow_odd": False,
+    "allow_empty": False,
+    "allow_delim_at_boundary": False,
+    "allow_non_utf8": False,
+    "use_latin1": False,
+    "allow_non_ascii_keywords": False,
+    "allow_missing_supp_text": False,
+    "allow_supp_text_own_delim": False,
+    "allow_missing_nextdata": False,
+    "trim_value_whitespace": False,
+    "ignore_standard_keys": _DEFAULT_KEY_PATTERNS,
+    "promote_to_standard": _DEFAULT_KEY_PATTERNS,
+    "demote_from_standard": _DEFAULT_KEY_PATTERNS,
+    "rename_standard_keys": {},
+    "replace_standard_key_values": {},
+    "append_standard_keywords": {},
+    "substitute_standard_key_values": ({}, {}),
+}
+
+STD_DEFAULT_CONFIG: dict[str, Any] = {
+    "trim_intra_value_whitespace": False,
+    "time_meas_pattern": _DEFAULT_TIME_MEAS_PATTERN,
+    "allow_missing_time": False,
+    "force_time_linear": False,
+    "ignore_time_optical_keys": [],
+    "date_pattern": None,
+    "time_pattern": None,
+    "allow_pseudostandard": False,
+    "allow_unused_standard": False,
+    "disallow_deprecated": False,
+    "fix_log_scale_offsets": False,
+    "nonstandard_measurement_pattern": None,
+    "ignore_time_gain": False,
+    "parse_indexed_spillover": False,
+    "disallow_localtime": False,
+}
+
+OFFSET_DEFAULT_CONFIG: dict[str, Any] = {
+    "text_data_correction": _DEFAULT_CORRECTION,
+    "text_analysis_correction": _DEFAULT_CORRECTION,
+    "ignore_text_data_offsets": False,
+    "ignore_text_analysis_offsets": False,
+    "allow_header_text_offset_mismatch": False,
+    "allow_missing_required_offsets": False,
+    "truncate_text_offsets": False,
+}
+
+LAYOUT_DEFAULT_CONFIG: dict[str, Any] = {
+    "allow_optional_dropping": False,
+    "transfer_dropped_optional": False,
+    "integer_widths_from_byteord": False,
+    "integer_byteord_override": None,
+    "disallow_range_truncation": False,
+}
+
+DATA_DEFAULT_CONFIG: dict[str, Any] = {
+    "allow_uneven_event_width": False,
+    "allow_tot_mismatch": False,
+}
+
+SHARED_DEFAULT_CONFIG: dict[str, Any] = {
+    "warnings_are_errors": False,
+    "hide_warnings": False,
+}
 
 
 @pytest.fixture
@@ -1832,6 +1924,86 @@ class TestLayouts:
         new = pf.MixedLayout(types)
         assert new.byte_widths == [32, 64, 8]
         assert new.typed_ranges == types
+
+
+class TestApiFunctions:
+    def test_read_header(self, tmp_path: Path, dataset2_3_2: pf.CoreDataset3_2) -> None:
+        d = tmp_path
+        d.mkdir(exist_ok=True)
+        p = d / "nonempty_dataset.fcs"
+        dataset2_3_2.write_text(p)
+        conf = HEADER_DEFAULT_CONFIG
+        _ = pf.api.fcs_read_header(p, **conf, dataset_offset=0)
+
+    def test_read_flat_text(
+        self, tmp_path: Path, dataset2_3_2: pf.CoreDataset3_2
+    ) -> None:
+        d = tmp_path
+        d.mkdir(exist_ok=True)
+        p = d / "nonempty_dataset.fcs"
+        dataset2_3_2.write_text(p)
+        conf = {**HEADER_DEFAULT_CONFIG, **FLAT_DEFAULT_CONFIG, **SHARED_DEFAULT_CONFIG}
+        _ = pf.api.fcs_read_flat_text(p, **conf, dataset_offset=0)
+        _ = pf.api.fcs_read_flat_texts(p, **conf)
+
+    def test_read_std_text(
+        self, tmp_path: Path, dataset2_3_2: pf.CoreDataset3_2
+    ) -> None:
+        d = tmp_path
+        d.mkdir(exist_ok=True)
+        p = d / "nonempty_dataset.fcs"
+        dataset2_3_2.write_text(p)
+        conf = {
+            **HEADER_DEFAULT_CONFIG,
+            **FLAT_DEFAULT_CONFIG,
+            **STD_DEFAULT_CONFIG,
+            **OFFSET_DEFAULT_CONFIG,
+            **LAYOUT_DEFAULT_CONFIG,
+            **SHARED_DEFAULT_CONFIG,
+        }
+        with pytest.RaisesGroup(pf.RelationalError, pf.ExtraKeywordError):
+            _ = pf.api.fcs_read_std_text(p, **conf, dataset_offset=0)
+            _ = pf.api.fcs_read_std_texts(p, **conf)
+
+    def test_read_flat_dataset(
+        self, tmp_path: Path, dataset2_3_2: pf.CoreDataset3_2
+    ) -> None:
+        d = tmp_path
+        d.mkdir(exist_ok=True)
+        p = d / "nonempty_dataset.fcs"
+        dataset2_3_2.write_text(p)
+        conf = {
+            **HEADER_DEFAULT_CONFIG,
+            **FLAT_DEFAULT_CONFIG,
+            **OFFSET_DEFAULT_CONFIG,
+            **LAYOUT_DEFAULT_CONFIG,
+            **DATA_DEFAULT_CONFIG,
+            **SHARED_DEFAULT_CONFIG,
+        }
+        _ = pf.api.fcs_read_flat_dataset(p, **conf, dataset_offset=0)
+        _ = pf.api.fcs_read_flat_datasets(p, **conf)
+
+    def test_read_std_dataset(
+        self, tmp_path: Path, dataset2_3_2: pf.CoreDataset3_2
+    ) -> None:
+        d = tmp_path
+        d.mkdir(exist_ok=True)
+        p = d / "nonempty_dataset.fcs"
+        dataset2_3_2.write_text(p)
+        conf = {
+            **HEADER_DEFAULT_CONFIG,
+            **FLAT_DEFAULT_CONFIG,
+            **STD_DEFAULT_CONFIG,
+            **OFFSET_DEFAULT_CONFIG,
+            **LAYOUT_DEFAULT_CONFIG,
+            **DATA_DEFAULT_CONFIG,
+            **SHARED_DEFAULT_CONFIG,
+        }
+        with pytest.RaisesGroup(pf.RelationalError, pf.ExtraKeywordError):
+            _ = pf.api.fcs_read_std_dataset(p, **conf, dataset_offset=0)
+            _ = pf.api.fcs_read_std_datasets(p, **conf)
+
+    # TODO add test for flat_dataset_from_kws
 
 
 class TestReadWrite:

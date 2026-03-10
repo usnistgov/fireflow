339aae304743f81595ab6e2f65042fad7f249528
njd2 <njd2@nist.gov>
2026-03-10 12:56:37 -0400

    ENH use items to check datepattern

--- crates/fireflow-core/src/validated/datepattern.rs
+++ crates/fireflow-core/src/validated/datepattern.rs
@@ -1,6 +1,9 @@
+use chrono::format::strftime::StrftimeItems;
+use chrono::format::{Fixed, Item, Numeric};
 use derive_more::{AsRef, Display};
-use std::str::FromStr;
 use thiserror::Error;
+
+use std::str::FromStr;
 
 #[cfg(feature = "python")]
 use fireflow_core_proc::{DisplayAsPyErr, FromPyString, IntoPyString};
@@ -10,7 +13,6 @@ use fireflow_core_proc::{DisplayAsPyErr, FromPyString, IntoPyString};
 /// This is a configuration value to be used when parsing a date using
 /// [`NaiveDate::parse_from_str`](chrono::NaiveDate::parse_from_str).
 #[derive(Clone, Debug, AsRef, Display)]
-#[as_ref(str)]
 #[cfg_attr(feature = "python", derive(FromPyString, IntoPyString))]
 pub struct DatePattern(String);
 
@@ -18,20 +20,39 @@ impl FromStr for DatePattern {
     type Err = DatePatternError;
 
     fn from_str(s: &str) -> Result<Self, DatePatternError> {
-        let count_spec = |spec: &'static str| s.match_indices(spec).count();
-        #[allow(non_snake_case)]
-        let nY = count_spec("%Y");
-        let ny = count_spec("%y");
-        let nm = count_spec("%m");
-        let nb = count_spec("%b");
-        #[allow(non_snake_case)]
-        let nB = count_spec("%B");
-        let nd = count_spec("%d");
-        let ne = count_spec("%e");
-        let y = matches!((nY, ny), (1, 0) | (0, 1));
-        let m = matches!((nm, nb, nB), (1, 0, 0) | (0, 1, 0) | (0, 0, 1));
-        let d = matches!((nd, ne), (1, 0) | (0, 1));
-        if y && m && d {
+        let mut year = 0_usize;
+        let mut month = 0_usize;
+        let mut day = 0_usize;
+        let mut invalid = 0_usize;
+
+        let pat = StrftimeItems::new(s)
+            .parse()
+            .map_err(|_| DatePatternError(s.to_owned()))?;
+
+        for item in &pat {
+            match item {
+                Item::Numeric(y, _) => match y {
+                    Numeric::Day => day += 1,
+                    Numeric::Month => month += 1,
+                    Numeric::Year | Numeric::YearMod100 => year += 1,
+                    Numeric::Internal(_) => debug_assert!(false, "this should never happen"),
+                    _ => invalid += 1,
+                },
+                Item::Fixed(y) => match y {
+                    Fixed::LongMonthName | Fixed::ShortMonthName => month += 1,
+                    Fixed::Internal(_) => debug_assert!(false, "this should never happen"),
+                    _ => invalid += 1,
+                },
+                Item::OwnedLiteral(_) | Item::OwnedSpace(_) | Item::Literal(_) | Item::Space(_) => {
+                }
+                // No errors because we parsed above.
+                Item::Error => {
+                    debug_assert!(false, "this should never happen");
+                }
+            }
+        }
+
+        if year == 1 && month == 1 && day == 1 && invalid == 0 {
             Ok(Self(s.into()))
         } else {
             Err(DatePatternError(s.into()))
@@ -60,5 +81,6 @@ mod tests {
         assert!("%yrandom%mmorerandom%d".parse::<DatePattern>().is_ok(),);
         assert!("%y%y%m%d".parse::<DatePattern>().is_err());
         assert!("%m%d".parse::<DatePattern>().is_err());
+        assert!("%H%y%m%d".parse::<DatePattern>().is_err());
     }
 }
